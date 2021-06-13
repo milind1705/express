2172382347944c612e7fc169748808bec48a5223
unknown <sois.milindsir@gmail.com>
2021-06-13 20:48:30 +0530

    welcome route

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express();
 app.get("/", function(req, res){
     res.send("Hello World");
 })
+app.get('/welcome', () => {
+    res.send('Welcome dear')
+})
 app.listen(3000, function(req, res){
     console.log('server is running')
 });
